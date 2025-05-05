3fdbd44bcd1cf09906943fff6a90b69d4b5d0432
S Pranav <129598591+deepuchary03@users.noreply.github.com>
2025-05-05 10:43:04 +0530

    Update 1464_Maximum_Product_of_Two_Elements_in_an_Array.java

--- 1464_Maximum_Product_of_Two_Elements_in_an_Array.java
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxProduct(int[] nums) {
         int max=0;
         for(int i=0;i<nums.length;i++){
-           int j= nums.length-i-1;
+            for(int j=i+1;j<nums.length;j++){
                 int temp = (nums[i]-1)*(nums[j]-1);
                 max=Math.max(temp,max);
-          
+            }
 
         }
 return max;
